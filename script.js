b4964c91cbb85c5a83467b5aef566aff47b335eb
kimbopom <kimbopoms@gmail.com>
2024-10-03 00:41:54 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    // Create a list of email addresses
+    var emails = [
+        "pptipse@gmail.com",
+        "pptipse@gmail.com",
+        "pptipse@gmail.com",
+        "pptipse@gmail.com"
+    ];
+
+    // Generate a random index
+    var randomIndex = Math.floor(Math.random() * emails.length);
+
+    // Get a random email
+    var randomEmail = emails[randomIndex];
+
+    // Create a new paragraph element
+    var paragraph = document.createElement("p");
+
+    // Set the ID attribute of the paragraph element
+    paragraph.id = "your_id_here";
+
+    // Set the text content of the paragraph element
+    paragraph.textContent = randomEmail;
+
+    // Append the paragraph element to the placeholder div
+    document.getElementById("emailField").appendChild(paragraph);
+}
