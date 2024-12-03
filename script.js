f92eaca85248927af8226b31620dbb4bb991b94b
kimbopom <kimbopoms@gmail.com>
2024-12-04 01:55:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     // Create a list of email addresses
     var emails = [
-        "virial.pp@gmail.com",
-        "virial.pp@gmail.com",
-        "virial.pp@gmail.com",
-        "virial.pp@gmail.com"
+        "pp.virial@gmail.com",
+        "pp.virial@gmail.com",
+        "pp.virial@gmail.com",
+        "pp.virial@gmail.com"
     ];
 
     // Generate a random index
